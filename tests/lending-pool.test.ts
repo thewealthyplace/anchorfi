87e17f14406a4b7290d9ee4b052d93413c442062
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-14 07:55:35 +0100

    Add get-loan-event returns first borrow event test

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -313,4 +313,11 @@ describe("lending-pool", () => {
     expect(result).toBeOk(Cl.uint(2));
   });
 
+  it("retrieves the first borrow event from history", () => {
+    setupProtocol();
+    borrowLoan(wallet1);
+    const { result } = getLoanEvent(wallet1, 0);
+    expect(result).toBeOk(Cl.some(expect.anything()));
+  });
+
 });
