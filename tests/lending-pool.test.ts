be0cacd4bacc61449b731763498bb745800458de
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:02:00 +0100

    Add liquidation registry event query variant 2

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -1855,3 +1855,12 @@ describe("lending-pool", () => {
     const { result } = simnet.callReadOnlyFn("liquidation", "get-liquidation-event", [Cl.uint(0)], deployer);
     expect(result).toBeOk(expect.anything());
   });
+
+  it("liquidation registry event query variant 2", () => {
+    setupProtocol();
+    borrowLoan(wallet1);
+    simnet.callPublicFn("oracle", "set-price", [Cl.uint(500_000)], deployer);
+    liquidateLoan(wallet1);
+    const { result } = simnet.callReadOnlyFn("liquidation", "get-liquidation-event", [Cl.uint(0)], deployer);
+    expect(result).toBeOk(expect.anything());
+  });
