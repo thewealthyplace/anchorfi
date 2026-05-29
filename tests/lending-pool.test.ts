a15a2a61be59141127ec369f42ca7eb73279f3fd
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:03:38 +0100

    Add health factor edge test #11

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -2426,3 +2426,11 @@ describe("lending-pool", () => {
     const { result } = getHealthFactor(wallet2);
     expect(Number((result as any).value.value)).toBeGreaterThan(0);
   });
+
+  it("health factor edge test batch5-11", () => {
+    setupProtocol();
+    borrowLoan(wallet1);
+    simnet.callPublicFn("oracle", "set-price", [Cl.uint(1500000)], deployer);
+    const { result } = getHealthFactor(wallet1);
+    expect(Number((result as any).value.value)).toBeGreaterThan(0);
+  });
