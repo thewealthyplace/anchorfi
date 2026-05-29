532e7bcb29255f6877a9dd7e573462d620c93b5c
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:02:31 +0100

    Add vault operation sequence test 11

--- tests/collateral-vault.test.ts
+++ tests/collateral-vault.test.ts
@@ -323,3 +323,9 @@ describe("collateral-vault", () => {
     const { result } = simnet.callReadOnlyFn("collateral-vault", "get-vault", [Cl.principal(wallet1)], wallet1);
     expect(result).toBeOk(expect.anything());
   });
+
+  it("vault operation sequence 11", () => {
+    simnet.callPublicFn("collateral-vault", "deposit", [Cl.uint(1500000000)], wallet1);
+    const { result } = simnet.callReadOnlyFn("collateral-vault", "get-vault", [Cl.principal(wallet1)], wallet1);
+    expect(result).toBeOk(expect.anything());
+  });
