09c71578c34e83a36ecddf4d3892c486cf0652c9
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:04:33 +0100

    Add vault error scenario #6

--- tests/collateral-vault.test.ts
+++ tests/collateral-vault.test.ts
@@ -378,3 +378,8 @@ describe("collateral-vault", () => {
     const { result } = simnet.callPublicFn("collateral-vault", "withdraw", [Cl.uint(100)], wallet1);
     expect((result as any).type).toBe("err");
   });
+
+  it("vault error scenario #6", () => {
+    const { result } = simnet.callPublicFn("collateral-vault", "withdraw", [Cl.uint(100)], wallet1);
+    expect((result as any).type).toBe("err");
+  });
