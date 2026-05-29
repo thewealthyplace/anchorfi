f2431ab82c27171c4ce92001b22b06f47289a79e
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:03:55 +0100

    Add price cache test #3

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -2791,3 +2791,13 @@ describe("lending-pool", () => {
     const { result } = simnet.callReadOnlyFn("lending-pool", "get-max-borrow", [Cl.uint(COLLATERAL)], deployer);
     expect(result).toBeOk(expect.anything());
   });
+
+  it("price cache test #3", () => {
+    setupProtocol();
+    borrowLoan(wallet1);
+    for (let b = 0; b < 2; b++) {
+      simnet.callPublicFn("oracle", "set-price", [Cl.uint(STX_PRICE)], deployer);
+    }
+    const { result } = simnet.callReadOnlyFn("lending-pool", "get-max-borrow", [Cl.uint(COLLATERAL)], deployer);
+    expect(result).toBeOk(expect.anything());
+  });
