2f8a0d86ba3f10d4c538e7c47838ec8c5f95ca33
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:04:36 +0100

    Add configure authorization test #3

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -3600,3 +3600,9 @@ describe("lending-pool", () => {
       [Cl.principal(deployer), Cl.principal(deployer), Cl.principal(deployer), Cl.principal(deployer)], wallet1);
     expect(result).toBeErr(Cl.uint(400));
   });
+
+  it("configure test #3", () => {
+    const { result } = simnet.callPublicFn("lending-pool", "configure",
+      [Cl.principal(deployer), Cl.principal(deployer), Cl.principal(deployer), Cl.principal(deployer)], wallet1);
+    expect(result).toBeErr(Cl.uint(400));
+  });
