f56af3c796b65a9e41c561fef82a4d68a6785afa
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:02:15 +0100

    Add liquidation total counter scenario 4

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -2081,3 +2081,18 @@ describe("lending-pool", () => {
     const { result } = simnet.callReadOnlyFn("liquidation", "get-total-liquidations", [], deployer);
     expect(Number((result as any).value.value)).toBe(3);
   });
+
+  it("liquidation total counter variant 4", () => {
+    setupProtocol();
+    for (let j = 0; j < 4; j++) {
+      borrowLoan(wallet1);
+      simnet.callPublicFn("oracle", "set-price", [Cl.uint(500_000)], deployer);
+      liquidateLoan(wallet1);
+      if (j < 3) {
+        simnet.callPublicFn("oracle", "set-price", [Cl.uint(STX_PRICE)], deployer);
+        simnet.callPublicFn("collateral-vault", "deposit", [Cl.uint(COLLATERAL)], wallet1);
+      }
+    }
+    const { result } = simnet.callReadOnlyFn("liquidation", "get-total-liquidations", [], deployer);
+    expect(Number((result as any).value.value)).toBe(4);
+  });
