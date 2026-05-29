bd261818672f71426f10b423485a167af2ec6499
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:04:29 +0100

    Add error handling scenario #6

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -3518,3 +3518,8 @@ describe("lending-pool", () => {
     const { result } = simnet.callPublicFn("lending-pool", "borrow", [Cl.uint(0), Cl.uint(0)], wallet1);
     expect((result as any).type).toBe("err");
   });
+
+  it("error handling scenario #6 on lending-pool", () => {
+    const { result } = simnet.callPublicFn("lending-pool", "borrow", [Cl.uint(0), Cl.uint(0)], wallet1);
+    expect((result as any).type).toBe("err");
+  });
