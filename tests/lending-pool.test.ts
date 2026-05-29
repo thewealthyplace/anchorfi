ab81667c55cd2cc47d28144426b85820726cacd7
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:03:54 +0100

    Add liquidation registry integration test #23

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -2744,3 +2744,12 @@ describe("lending-pool", () => {
     const { result } = simnet.callReadOnlyFn("liquidation", "get-total-liquidations", [], deployer);
     expect(result).toBeOk(Cl.uint(1));
   });
+
+  it("liq reg integration test #23", () => {
+    setupProtocol();
+    borrowLoan(wallet1);
+    simnet.callPublicFn("oracle", "set-price", [Cl.uint(500_000)], deployer);
+    liquidateLoan(wallet1);
+    const { result } = simnet.callReadOnlyFn("liquidation", "get-total-liquidations", [], deployer);
+    expect(result).toBeOk(Cl.uint(1));
+  });
