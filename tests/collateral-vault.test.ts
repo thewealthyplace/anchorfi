fec41820f5d64d3fac8290cb3dcd70189730d615
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-30 00:04:51 +0100

    Add vault deposit verification test #28

--- tests/collateral-vault.test.ts
+++ tests/collateral-vault.test.ts
@@ -610,3 +610,9 @@ describe("collateral-vault", () => {
     const { result } = simnet.callReadOnlyFn("collateral-vault", "get-vault", [Cl.principal(wallet1)], wallet1);
     expect(result).toBeOk(expect.anything());
   });
+
+  it("vault deposit test #28", () => {
+    simnet.callPublicFn("collateral-vault", "deposit", [Cl.uint(770000000)], wallet1);
+    const { result } = simnet.callReadOnlyFn("collateral-vault", "get-vault", [Cl.principal(wallet1)], wallet1);
+    expect(result).toBeOk(expect.anything());
+  });
